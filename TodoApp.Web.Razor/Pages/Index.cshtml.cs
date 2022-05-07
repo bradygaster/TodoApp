2868f4bd29cc0d581e30e6a93800992816e4f2b2
bradygaster <bradyg@microsoft.com>
2022-05-06 17:21:50 -0700

    adding logging to try to figure it out

--- TodoApp.Web.Razor/Pages/Index.cshtml.cs
+++ TodoApp.Web.Razor/Pages/Index.cshtml.cs
@@ -26,15 +26,23 @@ namespace TodoApp.Web.Pages
 
         public async Task<IActionResult> OnPostCreateNewTodoAsync()
         {
+            _logger.LogInformation($"Inside OnPostCreateNewTodoAsync in Razor client about to post. New todo item title is '{NewTodo.Title}' - Is Model State Valid: {ModelState.IsValid}");
+
             if (!ModelState.IsValid)
             {
+                _logger.LogInformation("Model state was invalid. Refreshing todos.");
                 Todos = await _todoApiClient.GetTodos();
                 return Page();
             }
 
-            if(NewTodo != null)
+            if (NewTodo != null)
+            {
+                _logger.LogInformation($"Model state was valid. Logging new todo item with title of '{NewTodo.Title}'");
                 await _todoApiClient.CreateTodo(NewTodo);
+                _logger.LogInformation("Called API client without any errors.");
+            }
 
+            _logger.LogInformation("Redirecting back to Razor index page.");
             return RedirectToPage("./Index");
         }
 
